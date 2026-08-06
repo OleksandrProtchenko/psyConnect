817e68b93d76c231972884c52e3d0caccec9e812
Oleksandr Protchenko <alexdevelop92@gmail.com>
2026-08-06 22:13:05 +0300

    add modal and login/signup page

---
+++ app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <section>
+      <h1>Log In</h1>
+      <p>Full page login view for direct visits and refresh.</p>
+    </section>
+  );
+}
--- components/Header/Auth/Auth.tsx
+++ components/Header/Auth/Auth.tsx
@@ -1,11 +1,24 @@
+'use client';
+
 import { AppButton } from '@/components/ui/Button/Button';
 import css from './Auth.module.css';
+import { useRouter } from 'next/navigation';
 
 export default function Auth() {
+  const router = useRouter();
+
   return (
     <div className={css.auth}>
-      <AppButton>Log In</AppButton>
-      <AppButton variant="secondary">Sign Up</AppButton>
+      <AppButton onClick={() => router.push('/login')} data-btn="login">
+        Log In
+      </AppButton>
+      <AppButton
+        variant="secondary"
+        onClick={() => router.push('/signup')}
+        data-btn="signup"
+      >
+        Sign Up
+      </AppButton>
     </div>
   );
 }
