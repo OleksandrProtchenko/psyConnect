dacf49f3c3bc277aa066c45f63cd92fe0c34a486
Oleksandr Protchenko <alexdevelop92@gmail.com>
2026-08-12 23:26:22 +0300

    add hero stats section on home page with styles, without mobile style

--- components/Home/Stats/Stats.tsx
+++ components/Home/Stats/Stats.tsx
@@ -1,3 +1,26 @@
+import css from './Stats.module.css';
+
 export default function Stats() {
-  return null;
+  return (
+    <div className={css.heroStatsWrapper}>
+      <div className={`container ${css.heroStatsContainer}`}>
+        <ul className={css.heroStatsList}>
+          <li className={css.heroStatsItem}>
+            <h2 className={css.heroStatsItemTitle}>120+</h2>
+            <p className={css.heroStatsItemDescription}>
+              verifiend specialists
+            </p>
+          </li>
+          <li className={css.heroStatsItem}>
+            <h2 className={css.heroStatsItemTitle}>5,000+</h2>
+            <p className={css.heroStatsItemDescription}>happy clients</p>
+          </li>
+          <li className={css.heroStatsItem}>
+            <h2 className={css.heroStatsItemTitle}>10+</h2>
+            <p className={css.heroStatsItemDescription}>years of experience</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
